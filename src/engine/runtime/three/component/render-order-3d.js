0a1007b58bbd724187aff161d44468fc82b51826
hvu2005 <vunh@falcongames.com>
2026-01-08 17:58:13 +0700

    Update render-order-3d.js

--- src/engine/runtime/three/component/render-order-3d.js
+++ src/engine/runtime/three/component/render-order-3d.js
@@ -5,22 +5,4 @@ export class RenderOrder3D extends RenderOrder {
     constructor(order = 0) {
         super(order);
     }
-
-    _onAttach() {
-        const transform = this.gameObject.transform;
-
-        /** @param {Transform} parent */
-        const onParentChanged = (parent) => {
-            const renderOrder = parent.getComponent(RenderOrder3D);
-            this._setOrderInternal(renderOrder.order || 0, false);
-        };
-
-        this.offParentChanged = transform.on(Transform.PARENT_CHANGED, onParentChanged);
-    }
-
-    _onDestroy() {
-        if (this.offParentChanged) {
-            this.offParentChanged();
-        }
-    }
 }
